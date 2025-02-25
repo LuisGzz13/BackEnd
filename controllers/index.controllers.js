8285b32698747d9940c6b7bfd905c12629fb860e
Luis <a01722501@tec.mx>
2025-02-25 10:28:00 -0600

    Test

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,3 +1,5 @@
 
 export const getIndex = (req, res) => { res.send('Hello World from API'); }
 export const getPing  = (req, res) => { res.send('pong'); }
+
+console.log('index.controllers.js loaded');
