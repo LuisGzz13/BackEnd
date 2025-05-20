057997fb7693e86bbb764c0da6b4f912685dce9c
Luis Gonzalez <114023856+LuisGzz13@users.noreply.github.com>
2025-05-20 17:49:09 -0600

    register

--- backend/routes/login.routes.js
+++ backend/routes/login.routes.js
@@ -1,4 +1,4 @@
-import { login, checkAuth } from "../controllers/login.controllers.js";
+import { login, checkAuth, register } from "../controllers/login.controllers.js";
 import { authMiddleware } from "../utils/jwt.js";
 import express from "express";
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/", login);
 router.post("/login/", login);
 router.get("/check", authMiddleware, checkAuth);
+router.post("/register", register);
 
 export default router;
